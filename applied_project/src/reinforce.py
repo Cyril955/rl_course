977b8bf221ad92887b9510ef4a1fbd106a1121f6
Emiliennnn <emilien.coudurier@epfl.ch>
2026-05-08 17:34:56 +0200

    Structure changes for project

--- applied_project/src/reinforce.py
+++ applied_project/src/reinforce.py
@@ -7,12 +7,26 @@ import torch.optim as optim
 from torch.distributions import Categorical
 torch.manual_seed(0)
 
-def reinforce(env, policy, optimizer, early_stop=False, n_episodes=1000, max_t=1000, gamma=1.0, print_every=100):
+
+def reinforce(env, policy, optimizer, baseline_id=None, early_stop=False, n_episodes=1000, max_t=1000, gamma=1.0, print_every=100):
+    if baseline_id is not None:
+        if baseline_id == 1:
+            baseline = baseline_1
+        elif baseline_id == 2:
+            baseline = baseline_2
+        elif baseline_id == 3:
+            baseline = baseline_3
+        else:
+            raise ValueError("Invalid baseline_id. Please choose from 1, 2, or 3.")
+    else:
+        baseline = baseline_trivial
+
     scores_deque = deque(maxlen=100)
     scores = []
     for e in range(1, n_episodes):
         saved_log_probs = []
         rewards = []
+        baseline_values = []
         state = env.reset()
         # Collect trajectory
         for t in range(max_t):
@@ -21,6 +35,7 @@ def reinforce(env, policy, optimizer, early_stop=False, n_episodes=1000, max_t=1
             saved_log_probs.append(log_prob)
             state, reward, done, _ = env.step(action)
             rewards.append(reward)
+            baseline_values.append(baseline(state))
             if done:
                 break
         # Calculate total expected reward
@@ -29,13 +44,15 @@ def reinforce(env, policy, optimizer, early_stop=False, n_episodes=1000, max_t=1
 
         # Recalculate the total reward applying discounted factor
         discounts = [gamma ** i for i in range(len(rewards) + 1)]
-        R = sum([a * b for a,b in zip(discounts, rewards)])
+        rewards_to_go = [sum([discounts[j]*rewards[j+t] for j in range(len(rewards)-t) ]) for t in range(len(rewards))]
 
         # Calculate the loss
         policy_loss = []
-        for log_prob in saved_log_probs:
+        for i in range(len(saved_log_probs)):
+            log_prob = saved_log_probs[i]
+            G_centered = rewards_to_go[i] - baseline_values[i]
             # Note that we are using Gradient Ascent, not Descent. So we need to calculate it with negative rewards.
-            policy_loss.append(-log_prob * R)
+            policy_loss.append(-log_prob * G_centered)
         # After that, we concatenate whole policy loss in 0th dimension
         policy_loss = torch.cat(policy_loss).sum()
 
@@ -50,3 +67,15 @@ def reinforce(env, policy, optimizer, early_stop=False, n_episodes=1000, max_t=1
             print('Environment solved in {:d} episodes!\tAverage Score: {:.2f}'.format(e - 100, np.mean(scores_deque)))
             break
     return scores
+
+def baseline_trivial(state):
+    return 0.0
+
+def baseline_1(state):
+    return 0.0
+
+def baseline_2(state):
+    return 0.0
+
+def baseline_3(state):
+    return 0.0
